b67974032702214a4606fe5888fea099c0cfb52d
Tex-wz <tangzhilin6@outlook.com>
2017-06-11 00:16:06 +0800

    + spectre css , navigation bar , logo and user image

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
   <div id="app">
     <!-- navigation bar -->
-    <nav>
-      <div class="wrapper">
-        <div class="navigation">
-          <div><router-link to="/" exact><img class="logo" src="~public/logo.png" height="33" alt="logo"></router-link></div>
-          <ul>
-            <li><router-link to="/faq">FAQ</router-link></li>
-            <li><router-link to="/about">About us</router-link></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <header class="navbar">
+        <section class="navbar-section">
+            <a href="/" class="navbar-brand logo">
+                <img src="~public/logo.svg" height="40px">
+                <span class="blue-text">Doboom</span>
+            </a>
+        </section>
+        <section class="navbar-section">
+            <!-- user section(avatar) -->
+            <div class="dropdown dropdown-right">
+                <a href="#" class="avatar dropdown-toggle" tabindex="0">
+                    <img src="~public/user.png">
+                </a>
+                <ul class="menu">
+                    <li class="menu-item">
+                        <a href="#">Settings</a>
+                        <a href="#">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </section>
+    </header>
     <!-- transition for the main viewport -->
     <transition name="fade" mode="out-in">
       <main class="content" role="main">
@@ -24,7 +35,9 @@
     <swagness></swagness>
   </div>
 </template>
-
+<style lang="less" src="./styles/spectre-master/spectre.less"></style>
+<style lang="less" src="./styles/spectre-master/spectre-exp.less"></style>
+<style lang="less" src="./styles/spectre-master/spectre-icons.less"></style>
 <!-- handcrafted stylesheet, tend to make it leaner -->
 <!-- <style lang="sass" src="./styles/config.scss"></style> -->
 
@@ -37,5 +50,19 @@ export default {
 </script>
 
 <style>
-
+    .navbar{
+        width: 960px;
+        margin: 0 auto;
+        padding: 5px 0px;
+    }
+    .blue-text{
+        color: #2196F3;
+    }
+    .logo img ,.logo span{
+        vertical-align: middle;
+    }
+    .logo span{
+        font-size: 18px;
+        padding: 5px;
+    }
 </style>
